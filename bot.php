7b368cad7a56895ccb9ee1f98f323f07e1cf8d9d
GOLFIT22551011 <pap_golfloveza@hotmail.com>
2018-12-01 18:54:18 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -6,7 +6,7 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
-public $san='';
+public static  $san='';
 if (!is_null($events['ESP'])) {
 	
 	send_LINE($events['ESP']);
