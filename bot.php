e06ae6d6e4bd43d49ff9b576f80d4af7f6f34db3
GOLFIT22551011 <pap_golfloveza@hotmail.com>
2018-12-01 16:40:28 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -64,7 +64,7 @@ if($text == "สวัสดีแตงโต"){
         replyMsg($arrayHeader,$arrayPostData);
     }
     else if($text == "ลดน้ำหนึ่ง"){
-        $image_url = "https://i.pinimg.com/originals/cc/22/d1/cc22d10d9096e70fe3dbe3be2630182b.jpg";
+        $image_url = "https://www.dropbox.com/home/line?preview=download.jpg";
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "image";
         $arrayPostData['messages'][0]['originalContentUrl'] = $image_url;
