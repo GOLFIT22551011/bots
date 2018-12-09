9acb9902f60a2a8cd43dad18d8cc19331a56001f
GOLFIT22551011 <pap_golfloveza@hotmail.com>
2018-12-09 16:00:29 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -207,19 +207,51 @@ else if($text == "01.00" || $text == "01.01" ||  $text == "01.02" ||  $text == "
 
 
 
-function replyMsg($arrayHeader,$arrayPostData){
-        $strUrl = "https://api.line.me/v2/bot/message/reply";
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,$strUrl);
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
-        curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        $result = curl_exec($ch);
-        curl_close ($ch);
-    }
-   exit;
+//function replyMsg($arrayHeader,$arrayPostData){
+  //      $strUrl = "https://api.line.me/v2/bot/message/reply";
+    //    $ch = curl_init();
+      //  curl_setopt($ch, CURLOPT_URL,$strUrl);
+        //curl_setopt($ch, CURLOPT_HEADER, false);
+        //curl_setopt($ch, CURLOPT_POST, true);
+        //curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
+        //curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
+        //curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+        //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        //$result = curl_exec($ch);
+        //curl_close ($ch);
+    //}
+   //exit;
+function send_LINE($msg){
+ $access_token = '+RAgZsXSoIB12rh5ilBLg3BySGaIGHSvVROMcOJ9yw0B96H9VLORNgQs+a6Og5wS/MOplVEgqgYoVs5BosxYieMV5GGaOqnXhNrFje4NnnPhc04X57HVXsYDisV4JycZ2OovPF6jkSq6EHAN6xijpQdB04t89/1O/w1cDnyilFU='; 
+ 
+   
+  $messages = [
+        'type' => 'text',
+        'text' => $msg
+        //'text' => $text
+      ];
+      // Make a POST Request to Messaging API to reply to sender
+      $url = 'https://api.line.me/v2/bot/message/push';
+      $data = [
+        'to' => 'U83a5616b8fbc8a46e75065d20f8297ad',
+        'messages' => [$messages],
+      ];
+      $post = json_encode($data);
+      $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+      $result = curl_exec($ch);
+      curl_close($ch);
+      echo $result . "\r\n"; 
+}
+
+
+
+
 
 ?>
+
