3d80ba08651d5b9cfa5fbfc026e999ed9c79c41f
GOLFIT22551011 <pap_golfloveza@hotmail.com>
2018-12-09 16:11:36 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,45 +1,30 @@
-<?php // callback.php
-require "vendor/autoload.php";
-require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
-$access_token = '+RAgZsXSoIB12rh5ilBLg3BySGaIGHSvVROMcOJ9yw0B96H9VLORNgQs+a6Og5wS/MOplVEgqgYoVs5BosxYieMV5GGaOqnXhNrFje4NnnPhc04X57HVXsYDisV4JycZ2OovPF6jkSq6EHAN6xijpQdB04t89/1O/w1cDnyilFU=';
-
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-// Loop through each event
-foreach ($events['events'] as $event) {
-// Reply only when message sent is in 'text' format
-if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-// Get text sent
-$text = $event['source']['userId'];
-// Get replyToken
-$replyToken = $event['replyToken'];
-// Build message to reply back
-$messages = [
-'type' => 'text',
-'text' => $text
-];
-// Make a POST Request to Messaging API to reply to sender
-$url = 'https://api.line.me/v2/bot/message/reply';
-$data = [
-'replyToken' => $replyToken,
-'messages' => [$messages],
-];
-$post = json_encode($data);
-$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-$result = curl_exec($ch);
-curl_close($ch);
-echo $result . "\r\n";
+ <?php
+  
+function send_LINE($msg){
+ $access_token = '+RAgZsXSoIB12rh5ilBLg3BySGaIGHSvVROMcOJ9yw0B96H9VLORNgQs+a6Og5wS/MOplVEgqgYoVs5BosxYieMV5GGaOqnXhNrFje4NnnPhc04X57HVXsYDisV4JycZ2OovPF6jkSq6EHAN6xijpQdB04t89/1O/w1cDnyilFU='; 
+ 
+   
+  $messages = [
+        'type' => 'text',
+        'text' => $msg
+        //'text' => $text
+      ];
+      // Make a POST Request to Messaging API to reply to sender
+      $url = 'https://api.line.me/v2/bot/message/push';
+      $data = [
+        'to' => 'U83a5616b8fbc8a46e75065d20f8297ad',
+        'messages' => [$messages],
+      ];
+      $post = json_encode($data);
+      $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+      $ch = curl_init($url);
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+      $result = curl_exec($ch);
+      curl_close($ch);
+      echo $result . "\r\n"; 
 }
-}
-}
-echo "OK";
+?>
