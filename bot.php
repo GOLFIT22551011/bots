a4ca035abd71819db84c8ea4f0fa1d916052ace5
GOLFIT22551011 <pap_golfloveza@hotmail.com>
2018-12-12 20:24:04 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -54,10 +54,10 @@ echo "OK3";
 	 
     }
 	else if($_POST['stime'] != null && $_POST['etime'] != null && $_POST['scrnN'] == "setDuration"){
- 		//send_LINE(substr($_POST['stime'],0,2));
-	      	//send_LINE(substr($_POST['stime'],3,5));
-		//send_LINE(substr($_POST['etime'],0,2));
-		//send_LINE(substr($_POST['etime'],3,5));
+ 		send_LINE(substr($_POST['stime'],0,2));
+	      	send_LINE(substr($_POST['stime'],3,5));
+		send_LINE(substr($_POST['etime'],0,2));
+		send_LINE(substr($_POST['etime'],3,5));
 		send_LINE("จัดให้ค่าาา ตั้งเวลาลดน้ำเรียบร้อยแลัวค่ะ");
 	}
 ////////////////////////////////////////////////////
