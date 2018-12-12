67a4555dc2a6390808727f339c4610a4820ef7a5
GOLFIT22551011 <pap_golfloveza@hotmail.com>
2018-12-12 23:01:20 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -58,10 +58,12 @@ echo "OK3";
 	      	send_LINE(substr($_POST['stime'],3,5));
 		send_LINE(substr($_POST['etime'],0,2));
 		send_LINE(substr($_POST['etime'],3,5));
-		send_LINE($_POST['stime']);
+		send_LINE($_POST['stime']."' ถึง '" .$_POST['etime']);
 		
-		send_LINE($_POST['etime']);
 		send_LINE("จัดให้ค่าาา ตั้งเวลาลดน้ำเรียบร้อยแลัวค่ะ");
+		getMqttfromlineMsg($_POST['stime']);
+		getMqttfromlineMsg($_POST['etime']);
+		
 	}
 ////////////////////////////////////////////////////
     else if($text == "ดูพื้นที่ลดน้ำ"){
