2af61a85c9f5c99653a986eb647b3f4e8f9757d4
GOLFIT22551011 <pap_golfloveza@hotmail.com>
2018-12-12 15:56:47 +0700

    Update line.php

--- line.php
+++ line.php
@@ -25,6 +25,6 @@ function send_LINE($msg){
       curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
       $result = curl_exec($ch);
       curl_close($ch);
-      echo $result . "\r\n"; 
+      //echo $result . "\r\n"; 
 }
 ?>
