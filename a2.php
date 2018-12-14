6f1ed0953a3bc21b19f77bf3260d97f53acce647
GOLFIT22551011 <pap_golfloveza@hotmail.com>
2018-12-14 11:05:23 +0700

    Update a2.php

--- a2.php
+++ a2.php
@@ -4,13 +4,29 @@
   
             <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   
-            <title>Form Get Demo</title>
+            <title>ตั้งเวลา</title>
+          <style>
+    .button {
+    background-color: #4CAF50; /* Green */
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+}
+.button4 {border-radius: 12px;}
+</style>
   
         </head>
   
         <body>
   <div align="center">
-     <h1> ตั้งเวลาสำเร็จแลัวค่ะ<br>
+    <br>
+     <h1> ตั้งเวลารดน้ำสำเร็จแลัวค่ะ<br>
           เริ่ม
         <?php
           
@@ -24,6 +40,7 @@
   
         ?>
        </h1>
+    <button class="button button4">12px</button>
       </div>
         </body>
   
