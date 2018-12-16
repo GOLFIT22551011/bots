79ae535ed0489a32a374bd83b7265d08b27cd69f
GOLFIT22551011 <pap_golfloveza@hotmail.com>
2018-12-16 14:12:14 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -33,7 +33,7 @@ if (!is_null($events['events'])) {
 
 $Topic = "NodeMCU1" ;
 //$text = "Test";
-//  getMqttfromlineMsg($Topic,$text);
+  getMqttfromlineMsg($Topic,$text);
 echo "OK3";
      $accessToken = "+RAgZsXSoIB12rh5ilBLg3BySGaIGHSvVROMcOJ9yw0B96H9VLORNgQs+a6Og5wS/MOplVEgqgYoVs5BosxYieMV5GGaOqnXhNrFje4NnnPhc04X57HVXsYDisV4JycZ2OovPF6jkSq6EHAN6xijpQdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
     $content = file_get_contents('php://input');
