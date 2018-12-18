8af379d6c26851a9119765dc7673ae65e4ada986
GOLFIT22551011 <pap_golfloveza@hotmail.com>
2018-12-18 17:05:49 +0700

    Add files via upload

---
+++ 4LmA4LiB4LiB4LiU4Lit4LiU4LiB4Lib4Lit4LirZXdzZGZzZGU=.php
@@ -0,0 +1,56 @@
+<html>
+  
+        <head>
+  
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  
+            <title>ตั้งเวลา</title>
+          <style>
+    .button {
+    background-color: #4CAF50; /* Green */
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 26px;
+    margin: 4px 2px;
+    cursor: pointer;
+}
+.button4 {border-radius: 12px;}
+
+           
+            
+            
+            
+</style>
+          <script>
+function closeWindow() { self.opener=this; self.close();  }
+</script>
+  
+        </head>
+  
+        <body>
+  <div align="center">
+    <br>
+     <h1> ตั้งเวลารดน้ำสำเร็จแลัวค่ะ<br>
+          เริ่ม
+        <?php
+          
+            echo $_GET['stime'];
+  
+        ?>
+       -
+       <?php
+          
+            echo $_GET['etime'];
+  
+        ?>
+       </h1>
+    <button id="closeWindow()" class="button button4" style="width:200px;" onclick="closeWindow()">บันทึกข้อมูล</button>
+    
+      </div>
+        </body>
+  
+    </html>
