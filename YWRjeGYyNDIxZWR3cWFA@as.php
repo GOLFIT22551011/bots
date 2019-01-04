776425160356df298febfc5bf8a6beff2c1e4cee
GOLFIT22551011 <pap_golfloveza@hotmail.com>
2019-01-05 01:06:41 +0700

    Update YWRjeGYyNDIxZWR3cWFA@as.php

--- YWRjeGYyNDIxZWR3cWFA@as.php
+++ YWRjeGYyNDIxZWR3cWFA@as.php
@@ -39,24 +39,25 @@ if($_GET['path'] == "removetime" ){
 		
 		
 		
-		//echo '<script type="text/javascript">
-  		//var method = "post";
+		echo '<script type="text/javascript">
+  		var method = "post";
 		//var path = "https://golfais.herokuapp.com/updatetime.php"
-    		//var form = document.createElement("form");
-    		//form.setAttribute("method", method);
-    		//form.setAttribute("action", path);
-		//var hiddenField = document.createElement("input");
-		//hiddenField.setAttribute("type", "hidden");
-		//hiddenField.setAttribute("name", "time");
-		//hiddenField.setAttribute("value","12.99,12.34,12.99,14.34,99.99,12.23");	
+		var path = "https://golfais.herokuapp.com/test.php"
+    		var form = document.createElement("form");
+    		form.setAttribute("method", method);
+    		form.setAttribute("action", path);
+		var hiddenField = document.createElement("input");
+		hiddenField.setAttribute("type", "hidden");
+		hiddenField.setAttribute("name", "time");
+		  hiddenField.setAttribute("value","12.99,12.34,12.99,14.34,99.99,12.23");	
 		
 		
-           	//form.appendChild(hiddenField);
-    		//document.body.appendChild(form);
-    		//form.submit();
-		//</script>';		
+           	form.appendChild(hiddenField);
+    		document.body.appendChild(form);
+    		form.submit();
+		</script>';		
 		
-	//echo "OK";
+	echo "OK";
 		
 		//replyMsg($arrayHeader,$_POST['stime']);
 		
