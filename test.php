fb030b4a59498cff775f10131f481dffa6126401
GOLFIT22551011 <pap_golfloveza@hotmail.com>
2019-01-05 01:12:14 +0700

    Update test.php

--- test.php
+++ test.php
@@ -18,15 +18,7 @@ function send_LINEBOT($msg){
       $post = json_encode($data);
       $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
       $ch = curl_init($url);
-      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-      $result = curl_exec($ch);
-      curl_close($ch);
-     // echo $result . "\r\n"; 
-      echo '<script type="text/javascript">
+	echo '<script type="text/javascript">
   		  var method = "post";
 		   var path = "https://golfais.herokuapp.com/updatetime.php"
     	var form = document.createElement("form");
@@ -42,6 +34,15 @@ function send_LINEBOT($msg){
     		document.body.appendChild(form);
     		form.submit();
 		</script>';	
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+      $result = curl_exec($ch);
+      curl_close($ch);
+     // echo $result . "\r\n"; 
+      
  
  
 }
