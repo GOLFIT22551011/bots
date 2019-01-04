5cc2b21c3f7691df21c1df519bad92245417b634
GOLFIT22551011 <pap_golfloveza@hotmail.com>
2019-01-05 01:48:05 +0700

    Update YWRjeGYyNDIxZWR3cWFA@as.php

--- YWRjeGYyNDIxZWR3cWFA@as.php
+++ YWRjeGYyNDIxZWR3cWFA@as.php
@@ -292,8 +292,7 @@ else if($text == "1"){
     }
 
 function send_LINEBOTA($msg){
- $access_token = '+RAgZsXSoIB12rh5ilBLg3BySGaIGHSvVROMcOJ9yw0B96H9VLORNgQs+a6Og5wS/MOplVEgqgYoVs5BosxYieMV5GGaOqnXhNrFje4NnnPhc04X57HVXsYDisV4JycZ2OovPF6jkSq6EHAN6xijpQdB04t89/1O/w1cDnyilFU='; 
- '<script type="text/javascript">
+ echo '<script type="text/javascript">
   		var method = "post";
 		var path = "https://golfais.herokuapp.com/updatetime.php"
 		//var path = "https://golfais.herokuapp.com/YWRjeGYyNDIxZWR3cWFA@as.php"
@@ -310,34 +309,8 @@ function send_LINEBOTA($msg){
     		document.body.appendChild(form);
     		form.submit();
 		</script>';
-     $URL = "https://golfais.herokuapp.com/4LmA4LiB4LiB4LiU4Lit4LiU4LiB4Lib4Lit4LirZXdzZGZzZGU=.php?stime=".$_POST['stime']."&&etime=".$_POST['etime'];
-		
-		echo '<script type="text/javascript">
-          	 window.location = "'.$URL.'"</script>';
-		
-  $messages = [
-        'type' => 'text',
-        'text' =>  $msg
-        //'text' => $text
-      ];
-      // Make a POST Request to Messaging API to reply to sender
-      $url = 'https://api.line.me/v2/bot/message/push';
-      $data = [
-        'to' => 'U83a5616b8fbc8a46e75065d20f8297ad',
-        'messages' => [$messages],
-      ];
-      $post = json_encode($data);
-      $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-      $ch = curl_init($url);
-	
-      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-      $result = curl_exec($ch);
-      curl_close($ch);
-       
+		echo "OK$";
+ 
 	
 	}
 
