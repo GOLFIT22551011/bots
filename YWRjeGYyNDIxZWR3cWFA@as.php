432e28a0df6be95f2acd2ec3591cca3380719808
GOLFIT22551011 <pap_golfloveza@hotmail.com>
2019-01-05 01:10:35 +0700

    Update YWRjeGYyNDIxZWR3cWFA@as.php

--- YWRjeGYyNDIxZWR3cWFA@as.php
+++ YWRjeGYyNDIxZWR3cWFA@as.php
@@ -10,12 +10,12 @@ $content = file_get_contents('php://input');
  // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
-if (!is_null($events['ESP'])) {
+//if (!is_null($events['ESP'])) {
 	
-	send_LINE($events['ESP']);
+//	send_LINE($events['ESP']);
 	
-	 echo "OK";
-	}
+//	 echo "OK";
+//	}
 if (!is_null($events['TimeSN'])) {
 	
 	send_LINE($events['TimeSN']);
