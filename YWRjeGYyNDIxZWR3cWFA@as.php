be6c3971f313999e07c44d723c8af928a8bd9c91
GOLFIT22551011 <pap_golfloveza@hotmail.com>
2019-01-05 09:48:21 +0700

    Update YWRjeGYyNDIxZWR3cWFA@as.php

--- YWRjeGYyNDIxZWR3cWFA@as.php
+++ YWRjeGYyNDIxZWR3cWFA@as.php
@@ -69,7 +69,7 @@ if($_GET['path'] == "removetime" ){
 		hiddenField.setAttribute("type", "hidden");
 		hiddenField.setAttribute("name", "time");
 		//hiddenField.setAttribute("value","12.99,12.34,12.99,14.34,99.99,12.23");	
-		hiddenField.setAttribute("value",$messages);
+		hiddenField.setAttribute("value",$msg);
 		
            	form.appendChild(hiddenField);
     		document.body.appendChild(form);
