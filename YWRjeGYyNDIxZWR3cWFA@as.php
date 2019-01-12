59d6fbe71d6250b7d17b3b2102e0166292a82290
GOLFIT22551011 <pap_golfloveza@hotmail.com>
2019-01-12 17:01:14 +0700

    Update YWRjeGYyNDIxZWR3cWFA@as.php

--- YWRjeGYyNDIxZWR3cWFA@as.php
+++ YWRjeGYyNDIxZWR3cWFA@as.php
@@ -39,7 +39,7 @@ if($_GET['path'] == "removetime" ){
 		$arrayPostData['messages'][0]['text'] = "ต้นข้าว ยกเลิกให้แลัวค่ะ";
 		replyMsg($arrayHeader,$arrayPostData);
              getMqttfromlineMsg("NodeMCU1","DeleteTime");
-             $URL = "https://golfais.herokuapp.com/YWRjeGYyNDIxZWR3cWFA@as.php?stime=".$events['TimeSN'];
+             $URL = "https://golfais.herokuapp.com/YWRjeGYyNDIxZWR3cWFA@as.php?path=removetime";
 		
              echo '<script type="text/javascript">
                     window.location = "'.$URL.'"</script>';
