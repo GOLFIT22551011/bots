4441d582d8a3adb8df84193ffc0c736d1ca13dd6
GOLFIT22551011 <pap_golfloveza@hotmail.com>
2019-01-15 15:56:51 +0700

    Update updatetime.php

--- updatetime.php
+++ updatetime.php
@@ -6,7 +6,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-
 </head>
 <body ng-app="myApp">
 <div ng-controller="customersCtrl">
@@ -33,47 +32,7 @@
 <form action="https://golfais.herokuapp.com/updatetime.html" style="" method="POST">
 </form>
 
-<?php
-  
-  function send_LINE2L($msg){
-   $access_token = '+RAgZsXSoIB12rh5ilBLg3BySGaIGHSvVROMcOJ9yw0B96H9VLORNgQs+a6Og5wS/MOplVEgqgYoVs5BosxYieMV5GGaOqnXhNrFje4NnnPhc04X57HVXsYDisV4JycZ2OovPF6jkSq6EHAN6xijpQdB04t89/1O/w1cDnyilFU='; 
-   
-     
-    $messages = [
-          'type' => 'text',
-          'text' =>  $msg
-          //'text' => $text
-        ];
-        // Make a POST Request to Messaging API to reply to sender
-        $url = 'https://api.line.me/v2/bot/message/push';
-        $data = [
-          'to' => 'U83a5616b8fbc8a46e75065d20f8297ad',
-          'messages' => [$messages],
-        ];
-        $post = json_encode($data);
-        $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-        $ch = curl_init($url);
-       
-  
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        $result = curl_exec($ch);
-        curl_close($ch);
-        // $URL = "https://golfais.herokuapp.com/updatetime.php?stime=".$msg;
-          
-       
-        echo $msg; 
-        //  '<script type="text/javascript">
-        // window.location = "'.$URL.'"</script>';
-   
-   
-  }
-  
-  
-  ?> 
+ 
 </div>
  {{testTime}}   
  
@@ -87,7 +46,7 @@
     app.controller('customersCtrl', function($scope, $http) {
         var str = "<?php echo $_POST["time"]; ?>";
        // $scope.testTime = "<?php $content = file_get_contents('php://input'); $events = json_decode($content, true);  $events['settimeSE']?>";
-        $scope. = "<testTime?php  $content = file_get_contents('php://input'); $events = json_decode($content, true);  send_LINE2L($events['settimeSE']); echo $events['settimeSE'];?>";
+        $scope.testTime = "<?php require('send2.php'); $content = file_get_contents('php://input'); $events = json_decode($content, true);  send_LINE2($events['settimeSE']); echo $events['settimeSE'];?>";
        // $testTime =$events['settimeSE'];
         
         var res = str.split(",");
