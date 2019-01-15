861625d6ae4e3548bf716267f330a1f9817bf5ed
GOLFIT22551011 <pap_golfloveza@hotmail.com>
2019-01-15 12:02:23 +0700

    Update send2.php

--- send2.php
+++ send2.php
@@ -30,7 +30,7 @@ function send_LINE2($msg){
       // $URL = "https://golfais.herokuapp.com/updatetime.php?stime=".$msg;
 		
      
-      // //echo $result . "\r\n"; 
+      echo $result . "\r\n"; 
       //  '<script type="text/javascript">
       // window.location = "'.$URL.'"</script>';
  
