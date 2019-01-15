0c43af9c69fd524b32b8a03bcc02f69bd5460c2d
GOLFIT22551011 <pap_golfloveza@hotmail.com>
2019-01-15 14:46:19 +0700

    Update 4LmA4LiB4LiB4LiU4Lit4LiU4LiB4Lib4Lit4LirZXdzZGZzZGU=.php

--- 4LmA4LiB4LiB4LiU4Lit4LiU4LiB4Lib4Lit4LirZXdzZGZzZGU=.php
+++ 4LmA4LiB4LiB4LiU4Lit4LiU4LiB4Lib4Lit4LirZXdzZGZzZGU=.php
@@ -48,12 +48,7 @@ function closeWindow() { self.opener=this; self.close();  }
               
   
         ?>
-       <?php
-$file = fopen('Log.txt','a+')  or die("Unable to open file!");
-$str = "\r\nทดสอบเขียนข้อมูลลงในไฟล์ \r\nต่อจากข้อมูลเดิม\r\n";
-fwrite($file,$str);
-fclose($file);
-?>
+      
        </h1>
     <button id="closeWindow()" class="button button4" style="width:200px;" onclick="closeWindow()">บันทึกข้อมูล</button>
     
