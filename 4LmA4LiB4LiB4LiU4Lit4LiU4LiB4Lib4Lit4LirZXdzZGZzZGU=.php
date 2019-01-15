e597d7dd9915cd19da58f885b153e97c3bcb45ab
GOLFIT22551011 <pap_golfloveza@hotmail.com>
2019-01-15 17:51:24 +0700

    Update 4LmA4LiB4LiB4LiU4Lit4LiU4LiB4Lib4Lit4LirZXdzZGZzZGU=.php

--- 4LmA4LiB4LiB4LiU4Lit4LiU4LiB4Lib4Lit4LirZXdzZGZzZGU=.php
+++ 4LmA4LiB4LiB4LiU4Lit4LiU4LiB4Lib4Lit4LirZXdzZGZzZGU=.php
@@ -47,7 +47,7 @@
         ?>
       
        </h1>
-    <button onclick="javascript:window.open('','_self').close();">บันทึกข้อมูล</button>
+    <button style="width:200px;" onclick="javascript:window.open('','_self').close();">บันทึกข้อมูล</button>
  
     
       </div>
