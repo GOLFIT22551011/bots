3b2a9d2ebf0007b5f7a96f84dd7ea31a2b859967
GOLFIT22551011 <pap_golfloveza@hotmail.com>
2019-01-16 13:56:19 +0700

    Update updatetime.php

--- updatetime.php
+++ updatetime.php
@@ -66,7 +66,7 @@ if (!is_null($events['settimeSE'])) {
     app.controller('customersCtrl', function($scope, $http) {
         var str = "<?php echo $_POST["time"]; ?>";
        // $scope.testTime = "<?php $content = file_get_contents('php://input'); $events = json_decode($content, true);  $events['settimeSE']?>";
-        $scope.testTime = "<?php require('send2.php'); $content = file_get_contents('php://input'); $events = json_decode($content, true); send_LINE2($events['settimeSE']);  echo $events['settimeSE'];?>";
+       // $scope.testTime = "<?php require('send2.php'); $content = file_get_contents('php://input'); $events = json_decode($content, true); send_LINE2($events['settimeSE']);  echo $events['settimeSE'];?>";
        // $testTime =$events['settimeSE'];
        var res = str.split(",");
         var mainInfo = null;
