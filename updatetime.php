9368bd5f46838c878956ef48dc3db580e9d2bbe7
GOLFIT22551011 <pap_golfloveza@hotmail.com>
2019-01-16 11:37:57 +0700

    Update updatetime.php

--- updatetime.php
+++ updatetime.php
@@ -52,7 +52,7 @@
     app.controller('customersCtrl', function($scope, $http) {
         var str = "<?php echo $_POST["time"]; ?>";
        // $scope.testTime = "<?php $content = file_get_contents('php://input'); $events = json_decode($content, true);  $events['settimeSE']?>";
-        $scope.testTime = "<?php require('send2.php'); $content = file_get_contents('php://input'); $events = json_decode($content, true);  send_LINE2($events['settimeSE']); echo $events['settimeSE'];?>";
+        $scope.testTime = "<?php require('send2.php'); $content = file_get_contents('php://input'); $events = json_decode($content, true);if (!is_null($events['TimeSN'])) {  send_LINE2($events['settimeSE']);  echo 'OK';}  echo $events['settimeSE'];?>";
        // $testTime =$events['settimeSE'];
         
         var res = str.split(",");
