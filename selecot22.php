2c8cb8534f0f1b5040c3a7a69c5f0139e8f041c2
GOLFIT22551011 <pap_golfloveza@hotmail.com>
2019-02-27 14:02:01 +0700

    Update selecot22.php

--- selecot22.php
+++ selecot22.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";  //your user name for php my admin if in local most probaly it will be "root"
 $password = "22551011";  //password probably it will be empty
-$databasename = "mysql"; //Your db nane
+$databasename = "sot"; //Your db nane
 // Create connection
 $conn = new mysqli($servername, $username, $password,$databasename);
 // Check connection
