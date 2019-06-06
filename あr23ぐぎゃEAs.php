a32a4dd6eb81ed549c4ebb34f044faf60d4a839f
GOLFIT22551011 <pap_golfloveza@hotmail.com>
2019-06-06 15:49:43 +0700

    Update あr23ぐぎゃEAs.php

--- あr23ぐぎゃEAs.php
+++ あr23ぐぎゃEAs.php
@@ -10,28 +10,21 @@ function send_LINE($msg){
         'text' =>  $msg
         //'text' => $text
       ];
+	  
       // Make a POST Request to Messaging API to reply to sender
-      $url = 'https://api.line.me/v2/bot/message/reply';
+      $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
-        
+        'to' => 'U83a5616b8fbc8a46e75065d20f8297ad',
         'messages' => [$messages],
       ];
       $post = json_encode($messages);
       $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
       $ch = curl_init($url);
-	  curl_setopt($ch, CURLOPT_URL,$url);
-	  curl_setopt($ch, CURLOPT_HEADER, false);
-	  curl_setopt($ch, CURLOPT_POST, true);
-	  
-	  curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
       curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	  
-	  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	  
-	  
-      
-	  
+      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
       $result = curl_exec($ch);
       curl_close($ch);
       echo $result . "\r\n"; 
