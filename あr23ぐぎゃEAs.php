95ff9d96a97669f7549d93124d24eca9f1d08220
GOLFIT22551011 <pap_golfloveza@hotmail.com>
2019-06-06 15:19:04 +0700

    Update あr23ぐぎゃEAs.php

--- あr23ぐぎゃEAs.php
+++ あr23ぐぎゃEAs.php
@@ -13,17 +13,25 @@ function send_LINE($msg){
       // Make a POST Request to Messaging API to reply to sender
       $url = 'https://api.line.me/v2/bot/message/reply';
       $data = [
-       'to' => '',
+        
         'messages' => [$messages],
       ];
       $post = json_encode($data);
       $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
       $ch = curl_init($url);
-      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+	  curl_setopt($ch, CURLOPT_URL,$url);
+	  curl_setopt($ch, CURLOPT_HEADER, false);
+	  curl_setopt($ch, CURLOPT_POST, true);
+	  
+	  curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
       curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	  
+	  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	  
+	  
+      
+	  
       $result = curl_exec($ch);
       curl_close($ch);
       echo $result . "\r\n"; 
